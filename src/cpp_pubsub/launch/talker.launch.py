44e55468dfe9e892fefb89472b3c7a340b822866
Jannik <brundli@yahoo.de>
2023-10-28 17:39:45 +0200

    fix

---
+++ src/cpp_pubsub/launch/talker.launch.py
@@ -0,0 +1,15 @@
+from launch import LaunchDescription
+import launch_ros.actions
+
+def generate_launch_description():
+    return LaunchDescription([
+        launch_ros.actions.Node(
+                    package="cpp_pubsub",
+        executable="talker",
+        name='sim'),
+        
+        launch_ros.actions.Node(
+            package="cpp_pubsub",
+            executable="listener",
+            name='sim')
+    ])
